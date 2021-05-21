fc7ef7923626e7fa9b7462a3a97e729fb05ee683
Thorsten Schoel <schoel@gfa-bis.de>
2021-05-21 14:33:39 +0200

    Add failing component

---
+++ src/app/test.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: '<p>Lorem ipsum dolor sit amet consectetur.</p>',
+  styles: ['p { text-color: blue }']
+})
+export class TestComponent { }
